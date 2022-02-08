7452d4208299f09d74470957a7cf7f940f4e23c6
Kirill <perevyshink_1@mail.ru>
2022-02-08 14:08:48 +0600

    Add EF stuff
    - Install EF Core, EF tools, EF SQLite
    - Create DBContext
    - Create DTOs
    - Create DesignTimeDbContextFactory to manage connection string and options

---
+++ YourDictionaries/DTOs/DictionaryDTO.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace YourDictionaries.DTOs
+{
+    public class DictionaryDTO
+    {
+        public Guid Id { get; set; }
+        public string DictionaryName { get; set; }
+        public List<PhraseDTO> Phrases { get; set; }
+    }
+}
---
+++ YourDictionaries/DbContexts/YourDictionariesDesignTimeDbContextFactory.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace YourDictionaries.DbContexts
+{
+    public class YourDictionariesDesignTimeDbContextFactory : IDesignTimeDbContextFactory<YourDictionariesDbContext>
+    {
+        public YourDictionariesDbContext CreateDbContext(string[] args)
+        {
+            DbContextOptions options = new DbContextOptionsBuilder().UseSqlite("Data Sourse=yourdic.db").Options;
+            return new YourDictionariesDbContext(options);
+        }
+    }
+}
